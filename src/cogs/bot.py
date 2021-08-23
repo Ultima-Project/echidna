a44b1b3708eb95390b317ecdaaecf901ddb6c5c0
Wiichele <pappalardosystem@gmail.com>
2021-08-23 17:51:29 +0200

    bot: refactor, remove unused modules

--- src/cogs/bot.py
+++ src/cogs/bot.py
@@ -1,19 +1,18 @@
 import discord
-from discord import activity
 from discord.ext import commands
-from os import sys
-from datetime import datetime
 from main import PREFIX
+
 
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.Cog.listener()
     async def on_ready(self):
-        print('OK ({0})'.format(self.bot.user))
-        await self.bot.change_presence(status=discord.Status.online,activity=discord.Game(PREFIX+"HELP | github.com/Ultima-Project/echidna"))
-        
+        print(f'OK ({self.bot.user})')
+        await self.bot.change_presence(status=discord.Status.online, activity=discord.Game(PREFIX+'HELP | github.com/Ultima-Project/echidna'))
+
 
 def setup(bot):
     bot.add_cog(Events(bot))
